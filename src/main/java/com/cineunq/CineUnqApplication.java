7c0a6f19d6b353d518f78e3ab39d334b6b9f7b96
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2023-03-21 22:25:48 -0300

    Setup Inicial

---
+++ src/main/java/com/cineunq/CineUnqApplication.java
@@ -0,0 +1,13 @@
+package com.cineunq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineUnqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineUnqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineunq/CineUnqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineunq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineUnqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ce5b30c245d953626b69a49149a3ff0c48db1f57
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2023-03-25 10:11:11 -0300

    Buscador por nombre para peliculas

--- src/main/java/com/cineunq/InitData.java
+++ src/main/java/com/cineunq/InitData.java
@@ -37,7 +37,9 @@ public class InitData {
         asientoService.saveAsiento(a1);
         asientoService.saveAsiento(a2);
         Pelicula p = new PeliculaBuilder().withNombre("Avengers 1").withDescripcion("The Avengers").withDuracion(150).withImagen("https://http2.mlstatic.com/D_NQ_NP_888996-MLA32569507268_102019-O.jpg").withAsientos(List.of(a1,a2)).build();
+        Pelicula p2 = new PeliculaBuilder().withNombre("John Wick").withDescripcion("La ciudad de Nueva York se llena de balas cuando John Wick, un exasesino a sueldo, regresa de su retiro para enfrentar a los mafiosos rusos, liderados por Viggo Tarasov, que destruyeron todo aquello que él amaba y pusieron precio a su cabeza").withDuracion(114).withImagen("https://http2.mlstatic.com/D_NQ_NP_637824-MLA40163107899_122019-O.jpg").withAsientos(null).build();
         peliculaService.savePelicula(p);
+        peliculaService.savePelicula(p2);
     }
 
 }
--- src/main/java/com/cineunq/controllers/PeliculaController.java
+++ src/main/java/com/cineunq/controllers/PeliculaController.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/peliculas")
@@ -22,13 +24,24 @@ public class PeliculaController {
     private PeliculaService peliculaService;
 
     @GetMapping(produces = "application/json")
-    @Operation(
-            summary = "Retorna todas las peliculas",
-            description = "Todas las peliculas")
+    @Operation(summary = "Retorna todas las peliculas",description = "Todas las peliculas")
     @LogExecutionTime
-    public ResponseEntity<List<Pelicula>> getAllPeliculas() {
-        List<Pelicula> users = peliculaService.getAll();
-        return ResponseEntity.ok().body(users);
+    public ResponseEntity<List<Pelicula>> getAllPeliculas(@PathVariable String pelicula) {
+        List<Pelicula> peliculas = peliculaService.getAll();
+        return ResponseEntity.ok().body(peliculas);
+    }
+
+    @GetMapping(value = "/{pelicula}",produces = "application/json")
+    @Operation(summary = "Retorna una pelicula",description = "Devuelve una pelicula si existe")
+    //@LogExecutionTime
+    public ResponseEntity<?> getPeliculaByname(@PathVariable("pelicula") String nombrePelicula) {
+        Optional<Pelicula> pelicula = peliculaService.findByNombre(nombrePelicula);
+        if (pelicula.isPresent()){
+            return ResponseEntity.ok().body(pelicula.get());
+        }else{
+            //return ResponseEntity.status(404).body("No se a encontrado la pelicula solicitada");
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }

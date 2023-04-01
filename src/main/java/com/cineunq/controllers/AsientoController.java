a462047e69cb8316f6aa75dfc6be8a1ead115be5
miguelEq <miguelequilea18@gmail.com>
2023-03-31 21:39:39 -0300

    agrego cors

--- src/main/java/com/cineunq/controllers/AsientoController.java
+++ src/main/java/com/cineunq/controllers/AsientoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/asientos")
 @Tag(name = "Asientos", description = "Endpoints para los asientos")
+@CrossOrigin(origins= "*", allowedHeaders = "*")
 public class AsientoController {
 
     @Autowired
--- src/main/java/com/cineunq/controllers/PeliculaController.java
+++ src/main/java/com/cineunq/controllers/PeliculaController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +16,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/peliculas")
 @Tag(name = "Peliculas", description = "Endpoints para las peliculas")
+@CrossOrigin(origins= "*", allowedHeaders = "*")
 public class PeliculaController {
 
     @Autowired
